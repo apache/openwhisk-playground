b441222da71609a51589396e1f70375e281d5c5b
Matt Rutkowski <mrutkows@us.ibm.com>
2017-07-12 14:34:52 -0500

    Update README with badges and source files with ASF license headers. (#5)
    * Update README for ASF license and Travis badges.

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-const debug = require('debug')('openwhisk-playground-test')  
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
+const debug = require('debug')('openwhisk-playground-test')
 const assert = require('assert')
 
 var OpenWhiskPlayground = require("./openwhisk-playground.js")
@@ -33,7 +36,6 @@ function test() {
     debug('error=' + JSON.stringify(err, null, 2))
     assert.ok(false, ' failed '+ JSON.stringify(err));
   }
-
 
   // evaluate
   playground.setInjectLogRetrievalFailure(true)
